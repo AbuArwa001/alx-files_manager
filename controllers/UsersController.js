c7f9debc6ef2050ab47d88656ff05a3d9c98bb91
AbuArwa001 <khalfanathman12@yahoo.com>
2024-09-04 20:32:40 +0300

    chore: Remove redundant token check in getMe method

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -50,23 +50,12 @@ class UsersController {
 
   static async getMe(req, res) {
     const token = req.headers['x-token'];
-
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
     const key = `auth_${token}`;
     const userId = await redisClient.get(key);
-
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
     const user = await dbClient.getUserById(userId);
     if (!user) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
-
     return res.status(200).json({ id: user._id, email: user.email });
   }
 }
