7cdac6cfcd0f3a53e8a68cb90c4b56f3adf2eed7
AbuArwa001 <khalfanathman12@yahoo.com>
2024-09-04 04:03:25 +0300

    Refactor RedisClient class to use 'EX' option for set method

--- utils/redis.js
+++ utils/redis.js
@@ -2,13 +2,13 @@ const redis = require('redis');
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient();  // Create the client
-   
+    this.client = redis.createClient(); // Create the client
+
     // Attach the error event listener
     this.client.on('error', (err) => console.log(err));
 
     // Explicitly connect to Redis
-    this.client.on('connect', () => console.log(''));
+    this.client.on('connect', () => {});
   }
 
   isAlive() {
@@ -16,22 +16,42 @@ class RedisClient {
   }
 
   async get(key) {
-    const value = await this.client.get(key);
-    return value;
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
   }
 
   async set(key, value, duration) {
-    await this.client.set(key, value, 'EX', duration);
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
   }
 
   async del(key) {
-    try {
-      await this.client.del(key);
-    } catch (error) {
-      console.error('Delete failed:', error);
-    }
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      });
+    });
   }
 }
 
 const redisClient = new RedisClient();
+redisClient.client.on('connect', () => {});
 export default redisClient;
