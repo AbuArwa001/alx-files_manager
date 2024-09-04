70a3e03e42e5e5a58135c7b21ac9462d52339c67
AbuArwa001 <khalfanathman12@yahoo.com>
2024-09-04 12:58:45 +0300

    chore: Update file retrieval route to exclude unnecessary fields

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -90,7 +90,9 @@ class FilesController {
       if (!file) {
         return res.status(404).json({ error: 'Not found' });
       }
-      return res.status(200).json({ id: file._id, ...file });
+      return res.status(200).json({
+        id: file._id, ...file, _id: undefined, localPath: undefined,
+      });
     } catch (error) {
       console.error('Error getting file:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
